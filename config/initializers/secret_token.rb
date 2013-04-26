9749fbd617e4f44a9f4af523b6b3bd526b765b7b
kubo0180 <gibson02122569@gmail.com>
2013-04-26 11:37:01 +0900

    create a plainpx.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plainpx::Application.config.secret_token = '7ed5dea07385cdd0b0d23f2e910834677d23111b3647255ae433ac03520beee8f69a8f5dc4fba8040c7a3fa63aa7a856d3061b8661857692270260965b8662cc'
