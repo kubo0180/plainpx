b81e817e3de547813ba5c2739414c1e2d39aa725
inaz2 <inaz2@twitter>
2013-04-28 16:42:18 +0900

    fix gmaps

--- app/assets/javascripts/pxes.js
+++ app/assets/javascripts/pxes.js
@@ -2,19 +2,15 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-function $(eid) {
-  return document.getElementById(eid);
-}
-
 function GmapInitialize() {
-  var lat = parseFloat($("latitude").textContent), lng = parseFloat($("longitude").textContent);
+  var lat = parseFloat(document.getElementById("latitude").textContent), lng = parseFloat(document.getElementById("longitude").textContent);
   var latlng = new google.maps.LatLng(lat, lng);
   var mapOptions = {
     center: latlng,
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map($("gmap_canvas"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("gmap_canvas"), mapOptions);
 
   var marker = new google.maps.Marker({
     position: latlng,
